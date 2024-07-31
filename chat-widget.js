3d3825253d1f742d77cc55e34eaed30524ac4e97
ZYXAWER <calalas.negocios@gmail.com>
2024-07-31 13:20:21 -0400

    Create chat-widget.js

---
+++ chat-widget.js
@@ -0,0 +1,12 @@
+(function() {
+    const iframe = document.createElement('iframe');
+    iframe.src = 'https://raw.githubusercontent.com/ZYXAWER/chat-ui/main/chat.html';
+    iframe.style.width = '300px';
+    iframe.style.height = '400px';
+    iframe.style.position = 'fixed';
+    iframe.style.bottom = '10px';
+    iframe.style.right = '10px';
+    iframe.style.border = 'none';
+    iframe.style.zIndex = '1000';
+    document.body.appendChild(iframe);
+})();
